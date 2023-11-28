15e7acb2648fea7f85973e1ef1286ef484736555
sinitsynme <sinitsynme@outlook.com>
2023-11-28 23:23:35 +0400

    Initial commit

---
+++ src/main/java/ru/sinitsynme/turingapi/TuringApiApplication.java
@@ -0,0 +1,13 @@
+package ru.sinitsynme.turingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuringApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sinitsynme/turingapi/TuringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sinitsynme.turingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
